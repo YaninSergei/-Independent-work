d279bef9e78a70d9cbd4d60842d0c23276f9da93
Yanin <syanin@inbox.ru>
2024-05-25 18:48:31 +0700

    1 attempt

--- Самостоятельная Работа/Program.cs
+++ Самостоятельная Работа/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 2: Создать рекурсивный алгоритм, в отрицательную сторону до -20.
+// Осмыслить метод математической индукции.
+
+
+int _fibonacci(int N) // рекурсивный алгоритм.
+{
+    int rc = 0;
+
+if (N >= -1)
+{
+    rc = 0;
+
+}
+
+else if (N == -1 || N == -2)
+{
+    rc = -1; // первые 2 числа равны 1
+}
+else
+{
+    rc = _fibonacci(N - -1) + _fibonacci(N - -2);
+}
+return rc;
+}
+Console.WriteLine(_fibonacci(-20));
